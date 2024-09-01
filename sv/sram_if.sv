deaf8c4451b40a4f46b2c5d13b06edaeb3b72508
wade <72756153+codeadpool@users.noreply.github.com>
2024-09-01 18:21:39 -0400

    [sv]: updated intf

--- sv/sram_if.sv
+++ sv/sram_if.sv
@@ -1,31 +1,53 @@
-interface sram_if(input clk, input rstn);
-  
- // Signals to SRAM  
-  logic [14:0] addr;
+interface sram_interface(
+  input logic clk,
+  input logic rstn
+);
+  // Signals to SRAM
+  logic [14:0]  addr;
   logic [255:0] din;
-  bit wen;
-
+  logic [255:0] dout;
+  logic we;
+  
   // Signals for Monitoring
-  bit mon_start, drv_start;
+  logic valid_tx;    
 
   task sram_reset();
     @(negedge rstn);
-    din <= 'hz;
-    disable send_to_dut;
+    din  <= 'hz;
+    addr <= 'hz;
+    we   <= 1'b0;
+    valid_tx <= 1'b0;
   endtask
 
-  task send_to_dut(
-    input bit [14:0]  address,
-    input bit [255:0] datain;
-    input bit         write_enable;
-    input int         packet_delay;
-    );
-
-    repeat(packet_delay) @(posedge clk); 
-    // @(posedge clk iff(!busy));
+  task write_to_sram(
+    input logic [14:0] address,
+    input logic [255:0] datain,
+    input bit write_enable
+  );
+    @(negedge clk);
     addr <= address;
     din  <= datain;
     we   <= write_enable;
+    valid_tx <= 1'b1;
+
+    // Wait for completion of the write operation
+    @(negedge clk);   
+    valid_tx <= 1'b0; 
+  endtask
+
+  task read_from_sram(
+    input logic [14:0] address,
+    output logic [255:0] data_out
+  );
+    @(negedge clk);
+    addr <= address;
+    we   <= 1'b0;    
+    valid_tx <= 1'b1;
+
+    // Wait for the read data to become valid
+    @(posedge clk);
+    data_out = dout;  // Capture the output data
+    valid_tx <= 1'b0; 
   endtask
 
 endinterface
