c287200b5edaff0d7a1a8d775c13eec4e2c4b3b9
codeadpool <72756153+codeadpool@users.noreply.github.com>
2024-09-10 10:05:48 -0400

    [sv]: updated agent, agent_config

--- sv/sram_tx_agent.sv
+++ sv/sram_tx_agent.sv
@@ -1,13 +1,15 @@
 class sram_tx_agent extends uvm_agent;
-  uvm_active_passive_enum is_active = UMV_ACTIVE;
-  
+  // uvm_active_passive_enum is_active = UVM_PASSIVE;
+  // by default this is passive;
+
   `uvm_component_utils_begin(sram_tx_agent)
     `uvm_field_enum(uvm_active_passive_enum, is_active, UVM_ALL_ON)
   `uvm_component_utils_end
 
-  sram_tx_monitor   monitor;
-  sram_tx_sequencer sequencer;
-  sram_tx_driver    driver;
+  sram_tx_monitor        m_mon0;
+  sram_tx_driver         m_drv0;
+  sram_tx_sequencer     m_seqr0;
+  sram_tx_agent_cnfg m_agnt_cfg;
 
   function new(string name = "sram_tx_agent", uvm_component parent);
     super.new(name, parent);
@@ -15,16 +17,16 @@ class sram_tx_agent extends uvm_agent;
 
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    monitor = sram_tx_monitor::type_id::create("monitor",this); 
+    m_mon0 = sram_tx_monitor::type_id::create("m_mon0",this); 
     if(is_active == UVM_ACTIVE) begin
-      sequencer = sram_tx_sequencer::type_id::create("sequencer", this);
-      driver    = sram_tx_driver::type_id::create("driver", this);
+      m_seqr0 = sram_tx_sequencer::type_id::create("m_seqr0", this);
+      m_drv0  = sram_tx_driver::type_id::create("m_drv0", this);
     end
   endfunction : build_phase
 
   virtual function void connect_phase(uvm_phase phase);
     if(is_active == UVM_ACTIVE) begin
-      driver.seq_item_port.connect(sequencer.item_export);
+      m_drv0.seq_item_port.connect(m_seqr0.item_export);
     end 
   endfunction : connect_phase
 
@@ -33,9 +35,9 @@ class sram_tx_agent extends uvm_agent;
   endfunction : start_of_simulation_phase
 
   function void assign_vi (virtual interface sram_if vif);
-    monitor.vif = vif;
+    m_mon0.vif = vif;
     if(is_active == UVM_ACTIVE)
-      driver.vif = vif;
+      m_drv0.vif = vif;
   endfunction      
 
 endclass
