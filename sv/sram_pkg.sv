386ef142cc2669f40cd945c66c0940e08716b73b
codeadpool <72756153+codeadpool@users.noreply.github.com>
2024-09-10 15:09:00 -0400

    [sv]: minor updates

---
+++ sv/sram_pkg.sv
@@ -0,0 +1,16 @@
+package sram_package;
+  import uvm_pkg::*;
+  `include "uvm_macros.svh"
+
+  `include "sram_packet.sv"
+  `include "sram_if.sv"
+
+  `include "sram_tx_monitor.sv"
+  `include "sram_tx_sequencer.sv"
+  `include "sram_tx_seqs.sv"
+  `include "sram_tx_driver.sv"
+  `include "sram_tx_agent_config.sv"
+  `include "sram_tx_agent.sv"
+  
+  `include "sram_env.sv"
+endpackage
